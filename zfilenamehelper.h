da4ed7a1e40e5777a1a3e67c0880c564e78a8c1f
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2018-02-13 22:04:05 +0100

    2

--- zfilenamehelper.h
+++ zfilenamehelper.h
@@ -9,7 +9,7 @@ public:
     zFileNameHelper();
 //    static QString getContextFilename(QString munkadir, QString tfname);
 //    static QString getCClassFilename(QString munkadir, QString adatbazisNev, QString tfname);
-    static QString append(QString h, QString p0, QString, QString);
+    static QString append(QString h, QString p0, QString = NULL, QString = NULL);
 };
 
 #endif // ZFILENAMEHELPER_H
