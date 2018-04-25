0652219826fba36e9e80a9371fc6b3defcf89af8
zoli <horvaath.zoltaan.debrecen@gmail.com>
2018-04-25 10:24:53 +0200

    6

--- globals.h
+++ globals.h
@@ -18,7 +18,7 @@ extern zTokenizer ztokenizer;
 extern QMap<QString, QString> globalCaptionMap;
 //extern QMap<QString, QVariant> typeMap;
 //extern QMap<QString, QVariant> typeMapR;
-//valami 5
+//valami 6
 
 extern QMap<QString, QString> typeMap;
 extern QMap<QString, QString> typeMapR;
